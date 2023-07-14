3cdf1d5c665e4078c788aec1cc8cfcff292294cc
harsha95harsha <123621407+harsha95harsha@users.noreply.github.com>
2023-07-14 18:17:16 +0530

    changes done in app.js file over mandatory fileds

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose
 // Set up Multer storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/') // Choose the directory to store the uploaded file
+    cb(null, './uploads') // Choose the directory to store the uploaded file
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname)
@@ -31,7 +31,7 @@ const storage = multer.diskStorage({
 })
 
 // Set up Multer upload
-const upload = multer({ storage })
+const upload = multer({ storage: storage })
 
 // Handle file upload
 app.post('/upload', upload.single('file'), (req, res) => {
@@ -42,20 +42,20 @@ app.post('/upload', upload.single('file'), (req, res) => {
   const jsonData = xlsx.utils.sheet_to_json(worksheet, { header: 1 })
 
   console.log(jsonData)
+
   // Remove the header row if present
   if (jsonData.length > 0 && Array.isArray(jsonData[0])) {
     jsonData.shift()
   }
-
   // Create an array of data objects to be inserted
   const dataToInsert = jsonData.map((row) => ({
     user_id: row[0], // Replace with the appropriate column index for each field
     user_name: row[1],
     Phone: row[2],
     location: row[3],
-    // ...
   }))
 
+  console.log(dataToInsert)
   // Insert the data into the database
 
   User.insertMany(dataToInsert)
@@ -67,30 +67,27 @@ app.post('/upload', upload.single('file'), (req, res) => {
       console.error('Error inserting data:', error)
       res.status(500).send('Error inserting data')
     })
+
+  const validUserData = dataToInsert.filter((user, index) => {
+    if (!user.user_id || !user.user_name) {
+      console.log(
+        `Validation failed for user at index ${
+          index + 2
+        }. Missing mandatory fields.`
+      )
+      return false
+    }
+    return true
+  })
+
+  if (validUserData.length !== dataToInsert.length) {
+    console.log('Some users failed validation.')
+  } else {
+    console.log(validUserData)
+  }
 })
 
 // Start the server
 app.listen(3000, () => {
   console.log('Server listening on port 3000')
 })
-
-// const connectDB = async (res) => {
-//   try {
-//     await client.connect()
-//     console.log('Connected to MongoDB.. ')
-
-//     const db = client.db('User_details')
-//     const collection = db.collection('Users')
-
-//     collection.insertMany(data, (err, result) => {
-//       if (err) {
-//         result.status(500).json({ msg: 'Failed to insert data' })
-//       } else {
-//         result.json({ msg: 'Data inserted succesfully' })
-//       }
-//     })
-//     client.close()
-//   } catch (err) {
-//     console.log('Error connecting to MongoDB Atlas', err)
-//   }
-// }
