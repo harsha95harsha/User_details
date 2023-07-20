3abecd8b6cc8cc91f90292f61467048a62fc628d
harsha95harsha <123621407+harsha95harsha@users.noreply.github.com>
2023-07-20 16:30:07 +0530

    added .env file

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const express = require('express')
 const multer = require('multer')
 const xlsx = require('xlsx')
 const mongoose = require('mongoose')
+const Joi = require('joi')
 const User = require('./models/users')
-
+require('dotenv').config()
 const app = express()
 
-const uri =
-  'mongodb+srv://Harsha:Nodedevpycube@nodeexpressprojetcs.lhoxcb4.mongodb.net/User_details?retryWrites=true&w=majority'
-
-// Replace <username>, <password>, <cluster>, and <database> with your actual values
-
 mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: false,
+    useUnifiedTopology: false,
+  })
   .then(() => {
     console.log('Connected to MongoDB Atlas')
   })
@@ -20,53 +19,34 @@ mongoose
     console.error('Error connecting to MongoDB Atlas:', error)
   })
 
-// Set up Multer storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads') // Choose the directory to store the uploaded file
+    cb(null, './uploads')
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname)
   },
 })
 
-// Set up Multer upload
 const upload = multer({ storage: storage })
 
-// Handle file upload
 app.post('/upload', upload.single('file'), (req, res) => {
   const workbook = xlsx.readFile('./User_details.xlsx')
-
-  const sheetName = workbook.SheetNames[0] // Assuming the data is in the first sheet
+  const sheetName = workbook.SheetNames[0]
   const worksheet = workbook.Sheets[sheetName]
   const jsonData = xlsx.utils.sheet_to_json(worksheet, { header: 1 })
 
-  console.log(jsonData)
-
-  // Remove the header row if present
   if (jsonData.length > 0 && Array.isArray(jsonData[0])) {
     jsonData.shift()
   }
-  // Create an array of data objects to be inserted
+
   const dataToInsert = jsonData.map((row) => ({
-    user_id: row[0], // Replace with the appropriate column index for each field
+    user_id: row[0],
     user_name: row[1],
     Phone: row[2],
     location: row[3],
+    status: row[4],
   }))
-
-  console.log(dataToInsert)
-  // Insert the data into the database
-
-  User.insertMany(dataToInsert)
-    .then(() => {
-      console.log('Data inserted successfully')
-      res.status(200).send('File uploaded and data inserted successfully')
-    })
-    .catch((error) => {
-      console.error('Error inserting data:', error)
-      res.status(500).send('Error inserting data')
-    })
 
   const validUserData = dataToInsert.filter((user, index) => {
     if (!user.user_id || !user.user_name) {
@@ -75,9 +55,12 @@ app.post('/upload', upload.single('file'), (req, res) => {
           index + 2
         }. Missing mandatory fields.`
       )
-      return false
+
+      user.status = `This user failed validation `
+    } else {
+      user.status = 'This user passed the validation'
     }
-    return true
+    return user
   })
 
   if (validUserData.length !== dataToInsert.length) {
@@ -85,9 +68,24 @@ app.post('/upload', upload.single('file'), (req, res) => {
   } else {
     console.log(validUserData)
   }
+  User.insertMany(validUserData)
+    .then((result) => {
+      console.log('Data inserted successfully:')
+      res.status(200).json({ msg: result })
+    })
+    .catch((error) => {
+      console.error('Error inserting data:')
+      res.status(500).json({ msg: error })
+    })
+  // const schema = Joi.object().keys({
+  //   user_id: Joi.number().required(),
+  //   user_name: Joi.string().required(),
+  //   Phone: Joi.number(),
+  //   location: Joi.string(),
+  //   status: Joi.string().allow(''),
+  // })
 })
 
-// Start the server
 app.listen(3000, () => {
   console.log('Server listening on port 3000')
 })
--- models/users.js
+++ models/users.js
@@ -1,18 +1,21 @@
+// const Joi = require('joi')
+// const joi = require('joi')
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  user_id: {
-    type: Number,
-  },
-  user_name: {
-    type: String,
-  },
-  Phone: {
-    type: Number,
-  },
-  location: {
-    type: String,
-  },
+  user_id: Number,
+  user_name: String,
+  Phone: Number,
+  location: String,
+  status: String,
 })
+
+// const schema = Joi.object().keys({
+//   user_id: Joi.number().required(),
+//   user_name: Joi.string().required(),
+//   Phone: Joi.number(),
+//   location: Joi.string(),
+//   status: Joi.string().allow(''),
+// })
 
 module.exports = mongoose.model('User', userSchema)
